c674a19edec414a75b9bd5b1046eacc4ea3ae5a3
TheWiseGhost <byjuaditya@gmail.com>
2024-03-19 14:50:04 -0500

    backend building

--- trainer_area.js
+++ trainer_area.js
@@ -1,15 +1,42 @@
-<!DOCTYPE html>
-<html lang='en-us'>
-    <meta charset="UTF-8">
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='trainer_area.css'>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+const url = new URL(location.href);
 
-    <head>
-    </head>
+const APILINK = 'https://1bb3d6a4-2ead-4c01-bf15-9a33d15ae568-00-2ljpumvkrxrk0.picard.replit.dev/'
 
-    <body>
+let refresh_button;
+let submit_button;
+let result_area;
+
+const main = document.getElementById('trainer_area');
+let answer;
+
+update(APILINK);
+
+async function set_problem(url) {
+  let p_arr = [];
+  url = url + "action/getting_problems";
+  const res = await fetch(url);
+  const data = await res.json();
+  data.forEach((problem) => {
+    p_arr.push(problem._problemId);
+  });
+  let problemId = p_arr[Math.floor(Math.random() * p_arr.length)];
+  console.log(problemId);
+  return problemId;
+}
+
+
+async function update(url){
+  let id = await set_problem(url);
+  url = url + id;
+  console.log(url);
+  console.log("id = " + id)
+  fetch(url).then(res => res.json())
+  .then(function(problem) {
+      console.log(problem);
+      img = problem.img;
+      answer = problem.answer;
+      div_main = document.createElement('div');
+      div_main.innerHTML = `
         <div class='trainer_area'>
             <div class='problem_type_title'>
                 Two Pole
@@ -36,7 +63,7 @@
 
             <div class='submit_button_area'>
                  <a href='trainer_portal.html'>
-                    <button class="blob-btn2" id='submit'>
+                    <button class="blob-btn2">
                         Submit
                         <span class="blob-btn2__inner">
                           <span class="blob-btn2__blobs">
@@ -52,7 +79,7 @@
 
             <div class='refresh_button_area'>
                  <a href='trainer_portal.html'>
-                    <button class="blob-btn" id='refresh'>
+                    <button class="blob-btn">
                         Refresh
                         <span class="blob-btn__inner">
                           <span class="blob-btn__blobs">
@@ -69,7 +96,31 @@
             <div class='result_area'>
 
             </div>
+      `;
 
-        </div>
-    </body>
-</html>
+      main.appendChild(div_main);
+      refresh_button = document.getElementById('refresh');
+      refresh_button.addEventListener('click', refresh_function);
+      result_area = document.getElementById('problem_text_area')
+
+      function refresh_function() {
+        main.innerHTML = ''
+        update(APILINK);
+      }
+
+      submit_button = document.getElementById('submit');
+      submit_button.addEventListener('click', submit_function);
+
+      function submit_function() {
+        let input_answer = document.getElementById('answer_input').value;
+        if (input_answer == answer) {
+          result_area.innerHTML = 'Correct!';
+        } else {
+          result_area.innerHTML = 'Incorrect!';
+        }
+
+      }
+  });
+}
+
+
