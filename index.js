d2bbde4c60575e85a21249a573457a04e2dffcdc
TheWiseGhost <byjuaditya@gmail.com>
2024-04-08 21:25:51 -0500

    fixed on start bug where login button wouldn't display as the storage variable login was none and not 'none'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let user = localStorage.getItem('user');
 const login_text = document.getElementById('login');
 
-if (user != 'none') {
+if (user != 'none' && user != '') {
     login_text.innerHTML = user;
 }
