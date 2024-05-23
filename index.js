350b5280c9316862e45879c6c44adae5a3bd21c8
TheWiseGhost <byjuaditya@gmail.com>
2024-05-22 21:51:45 -0500

    added profile page

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const login_text = document.getElementById('login');
 if (user != 'none' && user) {
     login_text.innerHTML = user;
 }
+
+login_text.onclick = () => {
+    if (user != 'none' && user) {
+        window.location.href = 'profile.html';
+    } else {
+        window.location.href = 'login.html';
+    }
+}
+
--- profile.js
+++ profile.js
@@ -1,58 +1,42 @@
 const url = new URL(location.href);
 
-//const APILINK = 'https://mathfirebackend.onrender.com/';
-const APILINK = 'http://localhost:8000/'
+const APILINK = 'https://mathfirebackend.onrender.com/';
+//const APILINK = 'http://localhost:8000/'
 
 
-const main = document.getElementById('profile_area');
+const main = document.getElementById('stats_area');
 
 let user = localStorage.getItem('user');
+let username = document.getElementById('username');
+username.innerHTML = user;
 
 
-async function update_leaderboard(url){
-    main.innerHTML = ''
-    url = url + 'competition/' + localStorage.getItem('competitionId');
-    fetch(url)
-    .then(res => res.json())
-    .then(function(element) {
-        div_header = document.createElement('div');
-        div_header.innerHTML = `
-            <div class='in_box_title'>
-                Leaderboard
-            </div>
-            <div class='leaderboard_container'>
-                <div class='leaderboard_grid'>
-                    <div class='leaderboard_grid_number' style='text-decoration: underline'>
-                        Rank
-                    </div>
-                    <div class='leaderboard_grid_name' style='text-decoration: underline'>
-                        Email
-                    </div>
-                    <div class='leaderboard_grid_score' style="text-decoration: underline">
-                        Score
-                    </div>
-                </div>
-            </div>
-            `
-        main.appendChild(div_header);
-        for (var i = 0; i < element.leaderboard_names.length; i++) {
-            div_main = document.createElement('div');
-            div_main.innerHTML = `
-                <div class='leaderboard_container'>
-                    <div class='leaderboard_grid'>
-                        <div class='leaderboard_grid_number'>
-                            ${i+1}
+async function update_stats(url){
+    url = url + 'competitions_search';
+    const res = await fetch(url);
+    const data = await res.json();
+    data.forEach((element) => {
+        if (element.status == 'completed') {
+            for (var i = 0; i < element.leaderboard_names.length; i++) {
+                if (user == element.leaderboard_names[i]) {
+                    div_main = document.createElement('div');
+                    div_main.innerHTML = `
+                        <div class='stats_container'>
+                            <div class='stats_grid'>
+                                <div class='stats_grid_title'>
+                                    ${element.title}
+                                </div>
+                                <div class='stats_grid_rank'>
+                                    ${i+1}
+                                </div>
+                            </div>
                         </div>
-                        <div class='leaderboard_grid_name'>
-                            ${element.leaderboard_names[i]}
-                        </div>
-                        <div class='leaderboard_grid_score'>
-                            ${element.leaderboard_scores[i]}
-                        </div>
-                    </div>
-                </div>
-            `
-            main.appendChild(div_main);
+                    `
+                    main.appendChild(div_main);
+                };
+            };
         };
     });
 }
+
+update_stats(APILINK);
