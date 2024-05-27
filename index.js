5cdbca3458e4fce426231a6ede7bd0941823c550
TheWiseGhost <byjuaditya@gmail.com>
2024-05-27 14:36:02 -0500

    fix login display bug

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let user = localStorage.getItem('user');
-const login_text = document.getElementById('login');
+const login_text = document.getElementById('login_text');
+const login = document.getElementById('login');
 
 if (user != 'none' && user) {
     login_text.innerHTML = user;
 }
 
-login_text.onclick = () => {
+login.onclick = () => {
     if (user != 'none' && user) {
         window.location.href = 'profile.html';
     } else {
